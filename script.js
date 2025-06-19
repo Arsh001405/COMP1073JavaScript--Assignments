36026718bef95c966b59e92ac8b8afc5abc8ba30
Arsh001405 <arshkahlon.14.2005@gmail.com>
2025-06-18 20:14:56 -0400

    Define story options and initialize selection array

---
+++ script.js
@@ -0,0 +1,8 @@
+// ===== Step 1: Define story parts options =====
+const options = [
+  ["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"],  // Part 1 options
+  ["sat on", "danced with", "saw", "doesn't like", "kissed"],                       // Part 2 options
+  ["a funny", "a scary", "a slimy", "a barking", "a fat"],                           // Part 3 options
+  ["goat", "monkey", "cow", "frog", "bug", "worm"],                                 // Part 4 options
+  ["on the moon", "on the chair", "in my spaghetti", "in my soup", "in my shoes"]    // Part 5 options
+];
